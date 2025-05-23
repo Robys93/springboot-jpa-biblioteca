471b2adcdcc0e264c81b5edfeafce0f31924d21b
Robys93 <rob.dicastro@gmail.com>
2025-05-23 13:11:27 +0200

    Aggiunti nuovi metodi a IBookService per supportare query aggiuntive

--- src/main/java/com/giuseppe/biblioteca/service/IBookService.java
+++ src/main/java/com/giuseppe/biblioteca/service/IBookService.java
@@ -10,4 +10,13 @@ public interface IBookService {
     BookDTO createBook(BookDTO bookDTO);
     BookDTO updateBook(Long id, BookDTO bookDTO);
     boolean deleteBook(Long id);
+
+    // Nuovi metodi aggiunti
+    List<BookDTO> findByAuthor(String author);
+    List<BookDTO> findByGenre(String genre);
+    List<BookDTO> findByTitleContaining(String title);
+    List<BookDTO> findByPublicationYearBefore(int year);
+    int countBooksByAuthor(String author);
+    List<BookDTO> findAllOrderByPublicationYearDesc();
+    List<BookDTO> findByTitleOrAuthor(String keyword);
 }
